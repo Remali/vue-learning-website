ead942cf91b1647b0b42a1ac49df8385ddfe682a
Remali Das <remalidas45@gmail.com>
2022-07-13 16:24:26 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #fff;
+  margin-top: 60px;
+  }
+body{
+    background: #000;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <div class="name">
+      <div class="letter">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      </div>
+      <div class="letter">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      </div>
+      <div class="letter">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      </div>
+      <div class="letter">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      </div>
+      <div class="letter">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      </div>
+      <div class="letter">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.name{
+  display: flex;
+}
+</style>
